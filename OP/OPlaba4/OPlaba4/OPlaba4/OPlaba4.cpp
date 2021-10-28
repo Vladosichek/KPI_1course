cbe5287680ab5a89fcfb49b44da27021b2f0677c
Vladosichek <vladprischepa2004@gmail.com>
2021-10-28 10:39:08 +0300

    I`ve done

---
+++ OP/OPlaba4/OPlaba4/OPlaba4/OPlaba4.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<cstdlib>
+#include<cmath>
+#include<float.h>
+using namespace std;
+
+int main()
+{
+	double a = 0;
+	double b = 0;
+	double c = 1.5;
+	double d = 0;
+	int n;
+	cout << "Enter n (n>=4):";
+	cin >> n;
+	if (n < 4)
+	{
+		cout << "Error";
+	}
+	else
+	{
+		for (long int i=4; i <= n; i+=1)
+		{
+			d = (i + 1) / (pow(i, 2) + 1) * c - a * b;
+			a = b;
+			b = c;
+			c = d;
+			cout << d << endl;
+		}
+		cout << "Speed is:" << d << endl;
+	}
+	return 0;
+	system("pause");
+}
