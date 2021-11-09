c25e5df690442624b2850726ce35e89e8354243e
Vladosichek <vladprischepa2004@gmail.com>
2021-11-09 12:14:29 +0200

    viuviu

--- OP/OPlaba6/C++/OPlaba6/OPlaba6/OPlaba6.cpp
+++ OP/OPlaba6/C++/OPlaba6/OPlaba6/OPlaba6.cpp
@@ -2,17 +2,7 @@
 #include <cmath>
 #include <iomanip>
 using namespace std;
-long double arctg(long double a, int l) {
-	int i = 1;
-	long double z=1;
-	long double result = 0;
-	while (fabs(z) >= pow(10, l)) {
-		z = pow(-1, i - 1) * pow(a, 2 * i - 1) / (2 * i - 1);
-		result += z;
-		i += 1;
-	} 
-	return result;
-}
+long double arctg(long double a, int l);
 int main(){
 	long double x;
 	int j;
@@ -24,7 +14,7 @@ int main(){
 	q = -j;
 	long double t, u, c;
 	if (x < 0){
-		cout << "Invalid x";
+		cout << "Invalid x"<< endl;
 	}
 	else if (x <= 1) {
 		cout << "y=arctg(x)+arctg(2x)" << endl;
@@ -33,16 +23,27 @@ int main(){
 		if (x <= 0.5) {
 			u = arctg(2 * x,j);
 			c = t+u;
-			cout << "arctg(2x)=" << fixed << setprecision(q) << u << endl << "y=" << fixed << setprecision(q) << c;
+			cout << "arctg(2x)=" << fixed << setprecision(q) << u << endl << "y=" << fixed << setprecision(q) << c<<endl;
 		}
 		else {
-			cout << "arctg(2x) can not be counted because taylor series for arctg(a) can be used if |a|<=1" << endl << "Error";
+			cout << "arctg(2x) can not be counted because taylor series for arctg(a) can be used if |a|<=1" << endl << "Error"<<endl;
 		}
 	}
 	else {
 		cout << "y=arctg(x)/arctg(x-5)" << endl;
-		cout << "arctg(x) can not be counted because taylor series for arctg(a) can be used if |a|<=1" << endl << "Error";
+		cout << "arctg(x) can not be counted because taylor series for arctg(a) can be used if |a|<=1" << endl << "Error"<<endl;
 	}
-	return 0;
 	system("pause");
+	return 0;
+}
+long double arctg(long double a, int l) {
+	int i = 1;
+	long double z = 1;
+	long double result = 0;
+	while (fabs(z) >= pow(10, l)) {
+		z = pow(-1, i - 1) * pow(a, 2 * i - 1) / (2 * i - 1);
+		result += z;
+		i += 1;
+	}
+	return result;
 }
