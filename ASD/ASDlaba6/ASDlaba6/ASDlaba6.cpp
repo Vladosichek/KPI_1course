cad6f8b7d4e3819280e76d0ebbe4e535965ecc17
Vladosichek <vladprischepa2004@gmail.com>
2021-11-16 19:52:12 +0200

    yeah boiii

---
+++ ASD/ASDlaba6/ASDlaba6/ASDlaba6.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+int trian(int u, int j, int n);
+int main() {
+	float n;
+	int a;
+	int b;
+	int c;
+	int p;
+	cout << "Enter n:";
+	cin >> n;
+	cout << "Pifagor's triangles:" << endl;
+	for (c = 1; c <= n; c += 1) {
+		for (b = 1; b <= c; b += 1) {
+			for (a = 1; a <= b; a += 1) {
+				p = trian(a, b, c);
+				if ( p == 1) {
+					cout << a << "|" << b << "|" << c << endl;
+				}
+			}
+		}
+	}
+	return 0;
+	system("pause");
+}
+int trian(int u, int j, int q){
+	int f;
+	f = 0;
+	if (u * u + j * j == q * q) {
+		f=1;
+	}
+	else {
+		f=0;
+	}
+	return f;
+}
---
+++ OP/OPlaba7/OPlaba7/OPlaba7/OPlaba7.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include<cmath>
+#include <time.h>
+using namespace std;
+void generate(float* p, int l);
+void output(float* p, int l);
+float special_average(float* p, int l, int e);
+void change(float* p, int l, int e, float g);
+int main() {
+	const int c = 100;
+	float M[c];
+	float q;
+	int k;
+	int n;
+	do {
+		cout << "Enter k(natural number):";
+		cin >>k;
+	} while (k < 1);
+	do {
+		cout << "Enter n(size of an array; from 1 to 100):";
+		cin >> n;
+	} while (n < 1 || n > 100);
+	generate(M, n);
+	output(M, n);
+	q = special_average(M, n, k);
+	cout << "Seeken average number:" << q << endl;
+	change(M, n, k, q);
+	output(M, n);
+	system("pause");
+}
+void generate(float* p,int l) {
+	srand(time(NULL));
+	for (int i = 0; i < l; i++) {
+		*p = (rand() % 30000 - 15000) / 100.0;
+		p += 1;
+	}
+}
+void output(float* p, int l) {
+	for (int i = 0; i < l; i++) {
+		cout << *p << " ";
+		p += 1;
+	}
+	cout << endl;
+}
+float special_average(float* p, int l, int e) {
+	float b;
+	int r = 0;
+	float sum = 0.0;
+	for (int i = 0; i < l; i++) {
+		if (*p > e) {
+			r += 1;
+			sum += *p;
+		}
+		p += 1;
+	}
+	b = sum / r;
+	cout << "Ammount of seeken numbers:" << r << endl;
+	return b;
+}
+void change(float* p, int l, int e, float g) {
+	for (int i = 0; i < l; i++) {
+		if (*p > e) {
+			*p = g;
+		}
+		p += 1;
+	}
+}
